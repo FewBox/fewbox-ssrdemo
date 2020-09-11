1626de9eeb211a9542b2388e82947631ed86f10f
Peter Pang <mv@live.cn>
2020-09-11 21:11:03 +0800

    Add footer.

---
+++ components/footer/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface IFooterProps {
+}
+
+class Footer extends React.Component<IFooterProps> {
+  public render() {
+    return (
+      <footer></footer>
+    );
+  }
+}
+
+export default Footer;
